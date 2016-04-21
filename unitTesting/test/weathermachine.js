0d28be51339e6b124f6640e887d9d7b498fae5fa
Greg Rozmarynowycz <crashtest1@live.com>
2016-04-21 00:13:29 -0400

    updates

--- unitTesting/test/weathermachine.js
+++ unitTesting/test/weathermachine.js
@@ -1,8 +1,10 @@
+"use strict";
+var expect = require('chai').expect;
 /**
  * Created by gjr8050 on 4/20/2016.
  */
 describe('WeatherMachine', function(){
-    "use strict";
-
-    
+    it("does stuff", function(){
+        expect(true).to.be.true;
+    });
 });
